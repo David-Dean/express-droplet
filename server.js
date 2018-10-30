b5001bdef2a5d066bd8a37e8bc1d71d7796e2c41
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-10-30 14:47:20 -0400

    newer version

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
 app.get('/foobar', function(req, res){
-    res.send("Hello World")
+    res.send("Hello World version 2")
 })
 app.listen(3040, function(){console.log('donzo on port 3040')})
